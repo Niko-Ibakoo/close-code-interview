16fa795290b38ac285c35ff032db6e35dd58110d
Niko <germano.niko01@gmail.com>
2023-03-08 10:08:39 -0500

    completed

--- src/App.js
+++ src/App.js
@@ -13,31 +13,49 @@ function App() {
   const [selected, setSelected] = useState([]);
   const [data, setData] = useState(Data);
   //get name function
-  const getName = (name)=>{
-    setSelected(current=>[...current, name])
-  }
-  //boxes 
-  const boxes = data.map(item=>(
-    <Boxes handleEvent={getName} props={item} key ={item.id}></Boxes>
-  ))
+  const getName = (name) => {
+    if (selected.includes(name) === false) {
+      setSelected((current) => [...current, name]);
+    } else if (selected.includes(name)) {
+      setSelected((existingName) => {
+        return existingName.filter((item) => item !== name);
+      });
+    }
+  };
+  //boxes
+  const boxes = data.map((item) => (
+    <Boxes handleEvent={getName} props={item} key={item.id}></Boxes>
+  ));
 
   return (
     <div className="App">
-    <h1 style={{textAlign:'center'}}>Close coding challenge</h1>
-    {selected[0]&& selected.map(item=>(
-      <span key={item}>{item}</span>
-    ))}
-      <div className="container">
-      {boxes}
-      </div>
-      <div style={{backgroundColor:'grey',marginTop:'30px',borderRadius:'10px', padding:'20px', fontSize:'22px'}}>
-        Hello Close Team, <br /><br />
-        I was having a hard time working on the code challange in the dafault IDE , I am not familiar with that IDE so I decided to recreate the code challange from scratch locally and then I delployed it on Netlify for you to review.
-        I hope that is not an issue, I have completed the task (hopefully I understood the assignment correctly, if not I apologize.)
+      <h1 style={{ textAlign: "center" }}>Close coding challenge</h1>
+      {selected[0] && selected.map((item) => <span key={item}>{item}</span>)}
+      <div className="container">{boxes}</div>
+      <div
+        style={{
+          backgroundColor: "grey",
+          marginTop: "30px",
+          borderRadius: "10px",
+          padding: "20px",
+          fontSize: "22px",
+        }}
+      >
+        Hello Close Team, <br />
         <br />
-        #I haven't included any edge cases as it was not a requirement, and I focused only on the logic, so it's ugly but it works! :) <br />
+        I was having a hard time working on the code challange in the dafault
+        IDE , I am not familiar with that IDE so I decided to recreate the code
+        challange from scratch locally and then I delployed it on Netlify for
+        you to review. I hope that is not an issue, I have completed the task
+        (hopefully I understood the assignment correctly, if not I apologize.)
         <br />
-        You can also check the code here : <a href="https://github.com/Niko-Ibakoo/close-code-interview.git">GitHub</a>
+        #I haven't included any edge cases as it was not a requirement, and I
+        focused only on the logic, so it's ugly but it works! :) <br />
+        <br />
+        You can also check the code here :{" "}
+        <a href="https://github.com/Niko-Ibakoo/close-code-interview.git">
+          GitHub
+        </a>
       </div>
     </div>
   );
